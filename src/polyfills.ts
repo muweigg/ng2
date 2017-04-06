2a6c04a379e2c38264855375c7c1cef19c54940e
Wei Mu <Wei Mu>
2017-04-06 16:37:13 +0800

    update

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -2,3 +2,7 @@ import 'ie-shim'; // Internet Explorer 9 support
 import 'core-js/es6';
 import 'core-js/es7/reflect';
 import 'zone.js/dist/zone';
+
+if (!PROD_ENV) {
+    require('zone.js/dist/long-stack-trace-zone');
+}
